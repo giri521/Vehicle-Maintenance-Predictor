72dd105350dad522d7645be9b4caf5df7da1b61c
VENNAPUSA GIRIVARDHAN REDDY <135786118+giri521@users.noreply.github.com>
2026-02-11 21:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,5 +266,5 @@ def logout():
     return redirect(url_for('main'))
 
 if __name__ == '__main__':
-    # Set host='0.0.0.0' for deployment
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
